2761f167a600a46721f7e757fc7e6090f4d5f4e4
胡志伟5 <huzhiwei5@hikvision.com>
2020-11-23 21:36:08 +0800

    添加会员信息查看功能

--- src/pages/memberDetail/index.js
+++ src/pages/memberDetail/index.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react';
+import Styles from '@styles/globalStyle.less'
+import { Avatar } from 'antd'
+import moment from '@utils/momentZH'
+import axios from 'axios'
 
 function MemberDetail(props){
-    console.log(props.match.params.id)
+    const [avatar,setAvatar] = useState(null)
+    const [username,setUsername] = useState('')
+    const [id,setId] = useState(0)
+    const [created,setCreated] = useState(null)
+    useEffect(()=>{
+        axios.get('/api/members/username/' + props.match.params.id).then(response=>{
+            setAvatar(response.data.avatar_large)
+            setUsername(response.data.username)
+            setId(response.data.id)
+            setCreated(response.data.created)
+        })
+    },[])
     return (
-        <div>MemberDetail</div>
+        <div className={Styles.memberContainer}>
+            <div className={Styles.box}>
+                <Avatar src={avatar}
+                            shape={'square'}
+                            size={82}
+                />
+                <div className={Styles.content}>
+                    <h1>{username}</h1>
+                    <div>V2EX 第 {id} 号会员</div>
+                    <div id={Styles.createdTime}>加入于 {created && moment(created*1000).format().split('T').join(' ')}</div>
+                </div>
+            </div>
+        </div>
     )
 }
 
